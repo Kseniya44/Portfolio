0f67832aabefee17f6e92181de56fdb7f2d79c07
kseniya <kseniya.for.email@gmail.com>
2021-01-21 18:23:12 +0200

    Nav was added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,10 @@
 console.log('file 1');
+
+const menuItem = document.querySelector(".nav__item_has_sub-nav");
+const subNav = document.querySelector(".sub-nav");
+menuItem.addEventListener("mouseover", () => {
+    subNav.classList.add("show-sub-nav");
+});
+menuItem.addEventListener("mouseout", () => {
+    subNav.classList.remove("show-sub-nav");
+});
